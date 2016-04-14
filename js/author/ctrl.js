2b06694b457a695247a696754b4b47a002c568f1
scoobster17 <scoobster17@hotmail.co.uk>
2016-04-14 15:00:07 +0100

    Added more text on multiple pages, slight styling and some comments

--- js/author/ctrl.js
+++ js/author/ctrl.js
@@ -2,8 +2,19 @@
 
 	angular.module('tailsApp')
 
-	.controller('authorCtrl', function($scope, $rootScope){
-		$scope.heading = 'hi author';
-	});
+	/**
+	 * Controller for Author page
+	 * @param  {[dependency]} $scope
+	 * @param  {[dependency]} TextFactory  Factory for retrieving text for page
+	 */
+	.controller('authorCtrl', ['$scope', 'TextFactory', function($scope, TextFactory){
+
+		// get page text
+		var textQuery = TextFactory.query();
+		textQuery.$promise.then(function(data) {
+			$scope.text = data[0].text.author;
+		});
+
+	}]);
 
 })();
--- js/home/ctrl.js
+++ js/home/ctrl.js
@@ -2,11 +2,20 @@
 
 	angular.module('tailsApp')
 
-	.controller('homeCtrl', ['$scope', '$rootScope', 'TextFactory', function($scope, $rootScope, TextFactory) {
+	/**
+	 * Controller for Home page
+	 * @param  {[dependency]} $scope
+	 * @param  {[dependency]} $rootScope
+	 * @param  {[dependency]} TextFactory  Factory for retrieving text for page
+	 */
+	.controller('homeCtrl', ['$scope', 'TextFactory', function($scope, TextFactory) {
+
+		// get page text
 		var textQuery = TextFactory.query();
 		textQuery.$promise.then(function(data) {
 			$scope.text = data[0].text.home;
 		});
+
 	}]);
 
 })();
